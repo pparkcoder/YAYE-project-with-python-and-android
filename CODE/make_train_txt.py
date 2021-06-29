c921f8b087f77e246a94f1561031742d0905427d
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2021-06-29 15:04:50 +0900

    update code file

--- CODE/make_train_txt.py
+++ CODE/make_train_txt.py
@@ -1,13 +1,23 @@
 # yolo 학습 시 train.txt 파일 만드는 코드
+# # /train_data/img\a.jpg 생성 -> # /train_data/img/a.jpg 변경
 
-f = open('./label/train.txt', 'r')
-
-while True:
-    data = f.readline()
-
-    if not data:
-        break
-
-    print('data/custom/images/'+data.strip('\n')+'.jpg')
-
+import glob
+filenames = []
+files = sorted(glob.glob("./train_data/img/*.jpg"))
+f = open("./train.txt",'a')
+for i in range(len(files)) :
+    f.write(files[i].lstrip('./') +'\n')
 f.close()
+# /train_data/img\a.jpg
+
+txt = open('train.txt','r')
+f = open('train2.txt','w') 
+while True : 
+    line = txt.readline() 
+    if not line: 
+        break 
+    temp = line.replace('\\','/')
+    f.write(temp) 
+txt.close()
+f.close()
+# /train_data/img/a.jpg
